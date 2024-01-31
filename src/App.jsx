341fc7a198ff439f24bfb58db2fc552e31d521c3
Andres <andres.antonio2000@hotmail.com>
2024-01-31 02:02:08 -0300

    Add movie search functionality and display movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import noResults from './mocks/no-results.json'
+import Movies from './Components/Movies'
+
+
 
 function App() {
 
+	let { Search: movies } = responseMovies
+	// movies = undefined
 
-  return (
-    <div>
-      <header>
-        <form action="GET">
-          <label htmlFor="movie-finder"> Buscar: <input type="text" name="movie" id="movie_finder" /></label>
-          <button type="submit">Buscar</button>
-        </form>
+	const mappedMovies = movies?.map(movie => ({
+		id: movie.imdbID,
+		title: movie.Title,
+		poster: movie.Poster
+	}))
+	
+	return (
+		<div>
+			<header>
+				<form action="GET">
+					<label htmlFor="movie-finder"> Buscar: <input type="text" name="movie" id="movie_finder" /></label>
+					<button type="submit">Buscar</button>
+				</form>
 
-      </header>
-    </div>
-  )
+			</header>
+
+			<Movies movies={mappedMovies}/>
+
+		</div>
+	)
 }
 
 export default App
---
+++ src/Components/Movies.jsx
@@ -0,0 +1,40 @@
+export function ListOfMovies({movies}) {
+        console.log(movies)
+		
+		return (
+			<>
+
+				{movies.map(movies => (
+						<div key={movies.id}>
+							<h2>{movies.title}</h2>
+							<img src={movies.poster} alt={movies.title} />
+						</div>
+					)
+				)
+				}
+			
+			</>
+		)
+	
+}
+
+export function NoMoviesResult() {
+	return (
+		<h2>No hay resultados</h2>
+	)
+}
+
+export default function Movies({movies}) {
+	const hasResults = movies && movies.length > 0
+	console.log(hasResults)
+	console.log(movies)
+	console.log(movies.length)
+	return (
+		<>
+			{hasResults ? <ListOfMovies movies={movies} /> : <NoMoviesResult />}
+		</>
+	)
+}
+
+
+
